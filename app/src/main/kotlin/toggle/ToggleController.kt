a3a4fa898e2804cf9e909dcba55b28ce1d4227b3
Pipe Alfaro <piperfe21@gmail.com>
2021-05-07 16:48:05 -0400

    FEATURE: add 404 scenario when repository doesn't have the name of toggle

--- app/src/main/kotlin/toggle/ToggleController.kt
+++ app/src/main/kotlin/toggle/ToggleController.kt
@@ -1,6 +1,8 @@
 package toggle
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 class ToggleController(@Autowired val toggleService: ToggleService) {
 
     @GetMapping("/feature-toggle/{name}")
-    fun resolveFeatureToggleValue(@PathVariable("name") name: String): Message =
-        Message(name, toggleService.getValue(name))
+    fun resolveFeatureToggleValue(@PathVariable("name") name: String) = try {
+        ResponseEntity
+            .ok()
+            .body(Message(name, toggleService.getValue(name)))
+    } catch (e: NoSuchElementException) {
+        ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(null)
+    }
 }
 
 data class Message(val name: String, val value: Boolean)
